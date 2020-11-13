f8c244635b39fd70a8bba1361525c25a9b2c0bb4
jawadefaj <jawadefaj006@gmail.com>
2020-11-13 15:45:11 -0800

    new Wheeled vehicle script

---
+++ Plugins/BT_Plugin/Source/BT_Plugin/BT_Vehicle/BTWheeledVehicle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "BTWheeledVehicle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BT_PLUGIN_API ABTWheeledVehicle : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
