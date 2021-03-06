a4f08de1dbf7d5a7d7b5856375ed15da15cf9a88
ishaan95 <ishaan.paranjape@gmail.com>
2021-03-05 18:29:46 -0800

    resolved some merge conflicts

--- Plugins/ParseXML/Source/ParseXML/Public/fileParser.h
+++ Plugins/ParseXML/Source/ParseXML/Public/fileParser.h
@@ -16,10 +16,7 @@ class UfileParser : public IFastXmlCallback
 {
 public:
 	UfileParser(const TCHAR*, FVector&);
-<<<<<<< HEAD
 	UfileParser(const TCHAR*);
-=======
->>>>>>> cf15187e4f22eb9283c2ec1c5dc1c0fd3121a4fd
 	virtual ~UfileParser();
 
 	//before starting any parsing.
@@ -35,11 +32,7 @@ public:
 
 	//Offset for all spawning coordinates used. Used in case of multiple road networks being spawned.
 	FVector multipleSpawningOffset;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> cf15187e4f22eb9283c2ec1c5dc1c0fd3121a4fd
 	//Spline Container
 	splineCont SplineContainer;
 	FString turnType = "";
